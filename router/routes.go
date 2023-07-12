94bc5f21a4637a6acdf7b7003357985df04b3bc6
matheusma678 <matheusma2510@hotmail.com>
2023-07-12 12:13:04 -0400

    feat: add createOpening handler logic

--- router/routes.go
+++ router/routes.go
@@ -6,6 +6,9 @@ import (
 )
 
 func initialzeRoutes(router *gin.Engine) {
+	// Initialize Handler
+	handler.InitializeHandler()
+
 	v1 := router.Group("/api/v1")
 	{
 		v1.GET("/opening", handler.ShowOpeningHandler)
